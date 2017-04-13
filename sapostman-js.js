3c732936dfbcd1448a6a015ff042a4d0b8c8f3c1
StephaniesHusband@gmail.com <StephaniesHusband@gmail.com>
2017-04-13 09:13:45 -0500

    added sapostman-js

---
+++ sapostman-js.js
@@ -0,0 +1,71 @@
+;(function (root, factory) {
+	if (typeof exports === "object") {
+		// CommonJS
+		module.exports = exports = factory();
+	}
+	else if (typeof define === "function" && define.amd) {
+		// AMD
+		define([], factory);
+	}
+	else {
+		// Global (browser)
+      root.SAPostmanJS = factory();
+	}
+}(this, function() {
+   var SAPostmanJS = SAPostmanJS || (function() {
+      return {
+         createTransaction: function(opts) {
+            var _privateKey;
+            var _encrypted;
+            var _clientId;
+
+            // Create our uri to hit and encode.
+            var uri = _.template('/sasec/ECustomerServices/rest/v1/<%=endpoint%>?_=<%=timestamp%>')({
+               endpoint: opts.endpoint,
+               timestamp: (new Date().getTime())
+            });
+
+            // Set the uri in our address above
+            postman.setEnvironmentVariable("uri", uri);
+
+            if (opts.privateKey) {
+               // Grab and encode the private key
+               _privateKey = opts.privateKey;
+
+               // Save for subsequent calls
+               postman.setEnvironmentVariable("privateKey", _privateKey);
+            }
+            else {
+               _privateKey = postman.getEnvironmentVariable("privateKey");
+            }
+
+            if (_privateKey) {
+               // Encrypt the uri using the private key
+               _encrypted = CryptoJS.AES.encrypt(uri, CryptoJS.enc.Utf8.parse(_privateKey), {
+                  iv: CryptoJS.enc.Hex.parse("0000000000000000"),
+                  keySize: 16,
+                  mode: CryptoJS.mode.CBC,
+                  padding: CryptoJS.pad.Pkcs7
+               });
+
+               // Set the SA_SIGNATURE values
+               _clientId = opts.clientId || postman.getEnvironmentVariable("clientId");
+
+               if (_clientId) {
+                  postman.setEnvironmentVariable("clientId", _clientId);
+
+                  postman.setEnvironmentVariable("clientKey", _encrypted.ciphertext.toString(CryptoJS.enc.Base64));
+               }
+               else {
+                  throw "No clientId";
+               }
+            }
+            else {
+               throw "No privateKey";
+            }
+         }
+      };
+   }());
+
+   return SAPostmanJS;
+}));
